bab5c86d6185823640eb2a5ef105c728db7e7220
poznas <misterdannypl@gmail.com>
2019-04-28 13:55:25 +0200

    login screen details

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -5,27 +5,59 @@ import {
   loginWithGoogleIdToken,
   refreshTokens
 } from "../connectors/AuthBackendConnector"
-import {View} from "react-native";
+import {ActivityIndicator, Image, View} from "react-native";
+import * as constant from './constants';
 
 class LoginScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false
+    }
+  }
 
   static get buttonTitle() {
     return "Sign-In with Google";
   }
 
   signInWithGoogle = async () => {
+    this.setState({isLoading: true});
     await GoogleConnector.signInWithGoogle().then(loginWithGoogleIdToken);
     await refreshTokens();
+    this.setState({isLoading: false});
   };
 
   render() {
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <GoogleSignInButton onPress={this.signInWithGoogle}>
-            {LoginScreen.buttonTitle}
-          </GoogleSignInButton>
+          <Image
+              style={{
+                alignSelf: 'center',
+                width: 150,
+                height: 150,
+                marginBottom: 20
+              }}
+              source={require('../../assets/bsm-logo.png')}/>
+          <Image
+              style={{
+                alignSelf: 'center',
+                width: 300,
+                height: 150,
+                marginBottom: 20
+              }}
+              source={require('../../assets/bsm-text.png')}/>
+          {this.renderSignInButton()}
         </View>
     );
+  }
+
+  renderSignInButton() {
+    if (this.state.isLoading) {
+      return <ActivityIndicator size={"large"} color={constant.mainColor}/>
+    }
+    return <GoogleSignInButton onPress={this.signInWithGoogle}>
+      {LoginScreen.buttonTitle}
+    </GoogleSignInButton>
   }
 }
 
